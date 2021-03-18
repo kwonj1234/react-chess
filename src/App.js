945774213a20758ba54878431795554071d656b5
jaekwon <kwonj1234@gmail.com>
2021-03-18 18:59:24 -0400

    begin working on chess board

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar'
+import Board from './components/Board'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <BrowserRouter>
+        <Switch>
+          <Route path="play">
+            <Board />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <span>
+        <button>Play Chess</button>
+      </span>
+    </div>
+  )
+}
