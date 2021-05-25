16a542db44937ce25681e8e37af3663b8d761157
jaekwon <kwonj1234@gmail.com>
2021-05-24 21:54:56 -0400

    create promotion modal files

--- src/components/PromotionModal/PromotionModal.js
+++ src/components/PromotionModal/PromotionModal.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 export default function PromotionModal(props) {
+
+  // Deconstruct props
+  const { isOpen, isWhite, onClose } = props;
+  
   return (
     <div>
       
