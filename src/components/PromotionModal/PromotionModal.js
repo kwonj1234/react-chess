43e63c882578b0dcbefce1475cc804ade8534d9b
jaekwon <kwonj1234@gmail.com>
2021-05-25 16:58:50 -0400

    add images to promotion modal

--- src/components/PromotionModal/PromotionModal.js
+++ src/components/PromotionModal/PromotionModal.js
@@ -5,6 +5,12 @@ export default function PromotionModal(props) {
   // Deconstruct props
   const { isOpen, isWhite, onClose } = props;
   
+  // Images for pieces
+  const bishopImage = isWhite ? "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg" : "https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg";
+  const knightImage = isWhite ? "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg" : "https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg";
+  const queenImage = isWhite ? "https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg" : "https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg";
+  const rookImage = isWhite ? "https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg" : "https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg";
+  
   return (
     <div>
       
